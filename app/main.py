c7b113aca8482fde6cd8750ca9319a27535a5c6d
Andrey Jekul <andrey.jekul@rhinocodes.com>
2026-07-03 15:38:52 +0300

    Py-copy-file completed

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+
+def copy_file(command: str) -> None:
+
+    str_parts = command.strip().split(" ")
+
+    if len(str_parts) == 3 and str_parts[0] == "cp":
+        cmd, origin, destination = str_parts
+
+        if origin != destination:
+            try:
+                with (open(origin, "r") as f,
+                      open(destination, "w") as d):
+
+                    d.write(f.read())
+
+            except FileNotFoundError:
+                pass
